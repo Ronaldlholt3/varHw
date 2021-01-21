ed99230d330e007bc4b461eef6ae6c2cf0dd10fc
Ronaldlholt3 <76957909+Ronaldlholt3@users.noreply.github.com>
2021-01-20 22:19:07 -0500

    var

---
+++ test.js
@@ -0,0 +1,39 @@
+var myName = "Ronald"
+document.write(Ronald);
+
+var myvar = 1;
+
+var myvar2 = 2;
+
+var myvar3 = 3;
+
+var myvar4 = 4;
+
+var myvar5 = 5;
+
+var myvar6 = 6;
+
+var myvar7 = 7;
+
+var myvar8 = 8;
+
+var myvar9 = 9;
+
+var myvar10 = 10;
+
+myvar + myvar2 = 3
+
+myvar + myvar3 = 4
+
+myvar9 * myvar6 = 54
+
+myvar5 * myvar9 = 45
+
+myvar7 - myvar3 = 4
+
+myvar8 - myvar7 = 1
+
+myvar10 / myvar2 = 5
+
+myvar4 / myvar2 = 2
+
